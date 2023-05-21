79de27e5d816b539bfd896ed83861a41037d2787
thegwan <gwan.email@gmail.com>
2023-05-21 14:46:21 -0700

    start feature isol scripts

---
+++ scripts/run_feature_isol.py
@@ -0,0 +1,125 @@
+import subprocess, re, os
+import toml
+import argparse
+
+
+# allowed % packet drop
+EPSILON=0.001
+def execute(cmd, executable):
+    stop = 0
+    popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ''):
+        print(stdout_line, end='')
+        if 'SW Dropped' in stdout_line:
+            num = re.findall('\d*\.*\d+\%', stdout_line)
+            if not num: continue
+            value = float(num[0].split('%')[0]) 
+            if value > EPSILON :
+                print(f'TERMINATING, current SW drops {value} greater than {EPSILON}...')
+                stream = os.popen(f'pidof {executable}')
+                pid = stream.read()
+                os.system(f'sudo kill -INT {pid}')
+                stop = 0      # continue decreasing buckets
+        elif 'DROPPED' in stdout_line:
+            num = re.findall('\d*\.*\d+\%', stdout_line)
+            if not num: continue
+            value = float(num[0].split('%')[0]) 
+            if value == 0:
+                # 0 drops
+                print('Zero drops...')
+                stop = 1
+            elif value <= EPSILON :
+                print(f'Epsilon {value}% dropped...')
+                stop = 2
+
+    popen.stdout.close()
+    popen.wait()
+    return stop
+
+def modify_binary(old_text, new_text):
+    with open('base_extract_features.rs', 'r', encoding='utf-8') as file:
+        filedata = file.read()
+    if old_text in filedata:
+        print(f'replacing {old_text} with {new_text}')
+    filedata = filedata.replace(old_text, new_text)
+    with open('/home/gerryw/retina/examples/extract_features/src/main.rs', 'w', encoding='utf-8') as file:
+        file.write(filedata)
+
+def compile_binary():
+    cmd = f'cargo build --release --bin extract_features --features timing'
+    popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ''):
+        if 'Compiling' in stdout_line or 'Finished' in stdout_line:
+            print(stdout_line, end='')
+    popen.stdout.close()
+    popen.wait()
+
+def main(args):
+    ft_names = [
+        'dur',
+        'proto',
+        's_bytes_sum',
+        'd_bytes_sum',
+        's_ttl_mean',
+        'd_ttl_mean',
+        's_load',
+        'd_load',
+        's_bytes_mean',
+        'd_bytes_mean',
+        's_pkt_cnt',
+        'd_pkt_cnt',
+        's_iat_mean',
+        'd_iat_mean',
+        'tcp_rtt',
+        'syn_ack',
+        'ack_dat',
+    ]
+
+    for feature in ft_names:
+        print(feature)
+        old_text = 'use retina_core::subscription::features::Features;'
+        new_text = f'use retina_core::subscription::features_{feature}::Features;'
+        modify_binary(old_text, new_text)
+        compile_binary()
+
+    # binary = args.binary
+
+    # duration = int(args.duration)
+    # start = int(args.start)
+    
+    # config_file = args.config
+    # outfile = args.outfile
+
+    # executable = f'/home/gerryw/retina/target/release/{binary}'
+    # cmd = f'sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH RUST_LOG=error {executable} -c {config_file} -o {outfile}'
+
+    # config=toml.load(config_file)
+    # n_cores = len(config['online']['ports'][0]['cores'])
+    # print(config)
+    # for b in range(start, 0, -n_cores):
+    #     print(f'Running {binary} with {b} buckets')
+        
+    #     config['online']['monitor']['log'] = None
+    #     config['online']['duration'] = duration
+    #     config['online']['ports'][0]['sink']["nb_buckets"] = b
+
+    #     f = open(config_file, 'w')
+    #     toml.dump(config, f)
+    #     f.close()
+
+    #     stop_code = execute(cmd, executable)
+    #     if stop_code > 0:
+    #         print(f'Stop code {stop_code}: done')
+    #         break
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--binary')
+    parser.add_argument('-d', '--duration')
+    parser.add_argument('-s', '--start')
+    parser.add_argument('-c', '--config')
+    parser.add_argument('-o', '--outfile')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    main(parse_args())
